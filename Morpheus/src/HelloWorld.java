83cca5bebf0bda7ab32bfd48ee5a8a4f69dc8e5f
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-24 15:42:17 +0100

    if

--- Morpheus/src/HelloWorld.java
+++ Morpheus/src/HelloWorld.java
@@ -3,26 +3,17 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		int number1 = 9;
-		int number2 = 1;
-		int sum = number1 + number2;
-		System.out.println(sum*sum);
+		boolean condition;
+		condition = true;
 		
-		char letter = 'R';
-		String lettero = "letter";
-		
-		String newLine = System.getProperty("line.separator");//This will retrieve line separator dependent on OS.
-
-		
-		System.out.println(letter + newLine + lettero);
-		
-		System.out.println(letter + "\n" + lettero);
-		
-		boolean bool;
-		
-		bool = true;
-		bool = false;
-		System.out.println(bool);
+		if (condition)
+		{
+			System.out.println("Die Aussage war wahr");
+		}
+		else
+		{
+			System.out.println("Die Aussage war falsch");
+		}
 
 	}
 
