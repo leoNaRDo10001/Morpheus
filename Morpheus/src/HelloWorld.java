2ea248cc27d49216c526ac0303be77b7b6129318
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-24 16:33:56 +0100

    switch case

--- Morpheus/src/HelloWorld.java
+++ Morpheus/src/HelloWorld.java
@@ -3,17 +3,44 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		boolean condition;
-		condition = true;
+		String Monat = "September";
 		
-		if (condition)
-		{
-			System.out.println("Die Aussage war wahr");
+		switch (Monat) {
+		case "Januar": System.out.println("I Quartal");
+		break;
+		case "Februar": System.out.println("I Quartal");
+		break;
+		case "Maerz": System.out.println("I Quartal");
+		break;
+		
+		case "April": System.out.println("II Quartal");
+		break;
+		case "Mai": System.out.println("II Quartal");
+		break;
+		case "juni": System.out.println("II Quartal");
+		break;
+		
+		case "Juli": System.out.println("III Quartal");
+		break;
+		case "August": System.out.println("III Quartal");
+		break;
+		case "September": System.out.println("III Quartal");
+		break;
+		
+		case "Oktober": System.out.println("IV Quartal");
+		break;
+		case "November": System.out.println("IV Quartal");
+		break;
+		case "Dezember": System.out.println("IV Quartal");
+		break;
+		
+		
+		default: System.out.println(Monat + " ist kein Monat");
+		
 		}
-		else
-		{
-			System.out.println("Die Aussage war falsch");
-		}
+		
+		
+		
 
 	}
 
