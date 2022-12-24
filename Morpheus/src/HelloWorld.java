85f07282d23c20d508c7c471ece1a544f460a6b5
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-24 14:55:22 +0100

    sum

--- Morpheus/src/HelloWorld.java
+++ Morpheus/src/HelloWorld.java
@@ -2,7 +2,11 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Wanna what?");
+		
+		int number1 = 9;
+		int number2 = 99;
+		int sum = number1 + number2;
+		System.out.println(sum);
 
 	}
 
