3e1703086f77394c4c7a50d79573e8dbc766f218
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-24 18:56:35 +0100

    array

--- Morpheus/src/HelloWorld.java
+++ Morpheus/src/HelloWorld.java
@@ -2,8 +2,16 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		for (int n = 0; n < 55; n++ ) {
-			System.out.println(n);
+		
+		int[] arr = new int[10];
+		
+		for (int n = 0; n < 10; n++ ) 
+		{
+			arr[n] = n + 10;
+		}
+		for (int i = 0; i < 10; i++) 
+		{
+			System.out.println(arr[i]);
 		}
 	}
 
