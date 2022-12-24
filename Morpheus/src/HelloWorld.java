0095843a4e89cfb1ea762b460b76e56e036fbec0
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-24 17:44:58 +0100

    do while

--- Morpheus/src/HelloWorld.java
+++ Morpheus/src/HelloWorld.java
@@ -3,45 +3,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		String Monat = "September";
+		int i = 0;
 		
-		switch (Monat) {
-		case "Januar": System.out.println("I Quartal");
-		break;
-		case "Februar": System.out.println("I Quartal");
-		break;
-		case "Maerz": System.out.println("I Quartal");
-		break;
-		
-		case "April": System.out.println("II Quartal");
-		break;
-		case "Mai": System.out.println("II Quartal");
-		break;
-		case "juni": System.out.println("II Quartal");
-		break;
-		
-		case "Juli": System.out.println("III Quartal");
-		break;
-		case "August": System.out.println("III Quartal");
-		break;
-		case "September": System.out.println("III Quartal");
-		break;
-		
-		case "Oktober": System.out.println("IV Quartal");
-		break;
-		case "November": System.out.println("IV Quartal");
-		break;
-		case "Dezember": System.out.println("IV Quartal");
-		break;
-		
-		
-		default: System.out.println(Monat + " ist kein Monat");
-		
-		}
-		
-		
-		
-
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 100);
 	}
 
 }
