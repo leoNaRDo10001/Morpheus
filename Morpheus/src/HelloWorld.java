aa4963dea835b978d31b68dc32763b6b5aeab2b3
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-24 15:26:06 +0100

    newLine in Sysout

--- Morpheus/src/HelloWorld.java
+++ Morpheus/src/HelloWorld.java
@@ -4,9 +4,17 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		int number1 = 9;
-		int number2 = 99;
+		int number2 = 1;
 		int sum = number1 + number2;
-		System.out.println(sum);
+		System.out.println(sum*sum);
+		
+		char letter = 'R';
+		String lettero = "letter";
+		
+		String newLine = System.getProperty("line.separator");//This will retrieve line separator dependent on OS.
+
+		
+		System.out.println(letter + newLine + lettero);
 
 	}
 
