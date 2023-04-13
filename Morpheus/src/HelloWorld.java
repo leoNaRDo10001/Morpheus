441d9104657fff0ea2f3ee63f159220f8dc2ed59
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2023-04-13 13:55:53 +0200

    method1

--- Morpheus/src/HelloWorld.java
+++ Morpheus/src/HelloWorld.java
@@ -1,21 +1,14 @@
 
-public class HelloWorld {
+public class HelloWorld 
+{
 
 	public static void main(String[] args) {
-		
-		int[] arr = new int[10];
-		int[] arr1 = new int[5];
-		
-		for(int zaehler = 0; zaehler < 5; zaehler ++)
-		{
-			arr[zaehler] = zaehler + 5;
-			arr1[zaehler] = arr[zaehler] * 10;
-		}
-		
-		for (int i = 0; i < 10; i++)
-		{
-			System.out.println(arr[i] * arr1[i % 5]);
-		}
+		blubb();
 	}
-/*comment lllllllllllll*/
+	
+	public static void blubb()
+	{
+		System.out.println("Lev");
+	}
+
 }
